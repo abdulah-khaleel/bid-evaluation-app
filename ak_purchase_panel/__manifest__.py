4334ae0316f95153abe895cd4314817b4a5db608
abdulah-khaleel <abdulah.khaleel@gmail.com>
2022-09-17 21:55:06 +0430

    [IMP] Improved the app description pages

--- ak_purchase_panel/__manifest__.py
+++ ak_purchase_panel/__manifest__.py
@@ -36,6 +36,7 @@
         'reports/bids_evaluation_summary.xml',
         'reports/bids_comparative_report.xml',
     ],
+    'images': ["static/description/banner-v15.png"],
     'license': 'LGPL-3',
     'application': False,
     'installable': True,
